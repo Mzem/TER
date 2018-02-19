ee6444f57314c9d0706a81728ed5e798f53bece5
idjir <idirhamad123@gmail.com>
2018-02-19 15:36:45 +0100

    rpng simple

--- app/src/main/exemples/exemples.c
+++ app/src/main/exemples/exemples.c
@@ -30,3 +30,9 @@ void essai_rsa(gmp_randstate_t r_state) {
 
 	mpz_clears(m1, m2, c, p, q, n, e, d, NULL);
 }
+
+void test_alea_premiers (mpz_t rpng,gmp_randstate_t r_state, int nb_bits_max)
+{
+    alea_premiers (rpng,r_state,nb_bits_max);
+    gmp_printf("Le nombre donné est: %Zd",rpng);	
+}
--- app/src/main/generateur_premier/rpng.c
+++ app/src/main/generateur_premier/rpng.c
@@ -1,1 +1,9 @@
 #include "rpng.h"
+
+void alea_premiers (mpz_t rpng,gmp_randstate_t r_state, int nb_bits_max)
+{	
+	int nb_reps = 15;
+	do {
+		mpz_rrandomb (rpng, r_state, nb_bits_max);
+	}while(isProbabPrime(rpng, nb_reps, r_state) == 0);		
+}
