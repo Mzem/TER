e3439b40deea43adfd1ef3aed2136257b2bbac8c
idjir <idirhamad123@gmail.com>
2018-02-20 19:03:25 +0100

    modif des bornes

--- app/src/main/generateur_premier/rpng.c
+++ app/src/main/generateur_premier/rpng.c
@@ -2,17 +2,31 @@
 
 void alea_premiers (mpz_t rpng,gmp_randstate_t r_state, int nb_bits_max)
 {	
-	//borne minimale de 64 bits
-	mpz_t nb_bits_min,deux;
+	//declaration et initialisation des variables
+	mpz_t nb_bits_min,deux,taille_rpng,tmp;
+	
 	mpz_init(nb_bits_min);
+	mpz_init(taille_rpng);
 	mpz_init(deux);
+	mpz_init(tmp);
 	mpz_set_ui(deux,2);
-	mpz_pow_ui(nb_bits_min,deux,64);
+	mpz_set(tmp,rpng);
+	
+	//nb_bits_min = 2^64
+	mpz_pow_ui(nb_bits_min,deux,64); 
+	
+	mpz_get_ui(tmp);//transformation du tmp en unsigned long int
+	mpz_pow_ui(taille_rpng,deux,tmp);
 	
 	//nb de répétitions du test de primalité
 	int nb_reps = 15;
 	
 	do {
 		mpz_rrandomb (rpng, r_state, nb_bits_max);
-	}while((isProbabPrime(rpng, nb_reps, r_state) == 0)||(mpz_cmp(rpng,nb_bits_min) < 0));		
+	}while((isProbabPrime(rpng, nb_reps, r_state) == 0)||(mpz_cmp(taille_rpng,nb_bits_min) < 0));		
+
+	mpz_clear(tmp);
+	mpz_clear(deux);
+	mpz_clear(nb_bits_min);
+	mpz_clear(taille_rpng);
 }
