f2d0b44a1be4e92fbe8718f75ae2299004cf16a7
idjir <idirhamad123@gmail.com>
2018-02-20 11:54:02 +0100

    rpng marche avec borne inf et borne sup

--- app/src/main/generateur_premier/rpng.c
+++ app/src/main/generateur_premier/rpng.c
@@ -2,8 +2,17 @@
 
 void alea_premiers (mpz_t rpng,gmp_randstate_t r_state, int nb_bits_max)
 {	
+	//borne minimale de 64 bits
+	mpz_t nb_bits_min,deux;
+	mpz_init(nb_bits_min);
+	mpz_init(deux);
+	mpz_set_ui(deux,2);
+	mpz_pow_ui(nb_bits_min,deux,64);
+	
+	//nb de répétitions du test de primalité
 	int nb_reps = 15;
+	
 	do {
 		mpz_rrandomb (rpng, r_state, nb_bits_max);
-	}while(isProbabPrime(rpng, nb_reps, r_state) == 0);		
+	}while((isProbabPrime(rpng, nb_reps, r_state) == 0)||(mpz_cmp(rpng,nb_bits_min) < 0));		
 }
