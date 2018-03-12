4724fa9c85dfe1fd95091b983b3f9ca744bf9dda
Utilisateur <user@debian.arcahe.ovh>
2018-03-12 11:52:11 +0100

    debut du test naif

---
+++ app/src/main/tests_primalite/naif.c
@@ -0,0 +1,11 @@
+#include "naif.h"
+
+int Naif(mpz_t n,  gmp_randstate_t etat)
+{
+	/*
+	 ce test est deterministe (retourne jamais 1)
+	 pour i de 2 a racine(n)
+		si i divise n return 0
+	 */
+	return 2;
+}
---
+++ app/src/main/tests_primalite/naif.h
@@ -0,0 +1,10 @@
+#ifndef NAIF_H
+#define NAIF_H
+
+#include <gmp.h>
+#include <time.h>
+
+int Naif(mpz_t n, gmp_randstate_t etat);
+
+#endif
+
