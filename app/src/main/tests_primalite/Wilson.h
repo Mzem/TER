0170d39a990fda796c327d830288f7b8b84117d2
younes bens <younesbenyamna@gmail.com>
2018-03-17 13:15:10 +0100

    Test Wilson fini

---
+++ app/src/main/tests_primalite/Wilson.h
@@ -0,0 +1,15 @@
+#ifndef WILSON_H
+#define WILSON_H
+
+#include <gmp.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void Fact(mpz_t retour,mpz_t n);
+int Wilson(mpz_t n);
+
+
+#endif
